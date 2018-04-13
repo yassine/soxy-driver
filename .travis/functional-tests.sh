151284108c4f953a175a835accd7af148f04c390
yassine <yechabbi@gmail.com>
2018-04-13 18:03:21 +0100

    travis environment debug

--- .travis/functional-tests.sh
+++ .travis/functional-tests.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 #Blocking DNS traffic system wide
+
+cat /etc/resolv.conf
 sudo iptables -I OUTPUT -p udp --dport 53 -j DROP
 sudo iptables -I INPUT -p udp --sport 53 -j DROP
-docker run --rm -it uzyexe/curl https://api.ipify.org
+sudo iptables -L -v
+
+dig www.google.com @ 8.8.8.8
 ec=$?
+
+echo "got exit status $ec"
+
 if [ $ec -eq 0 ]
 then
   #DNS resolution should have failed as dns is blocked
-  echo "got exit status $ec"
-  exit 1
+  echo ""
 fi
 
 #DNS should still be resolved by containers that use a network based on the driver as those requests are tunneled
